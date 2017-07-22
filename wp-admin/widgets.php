acb8a1495c1b4829100a967c2abc1d654fcce4ff
Nikos Athanasakis <emidekol@gmail.com>
2017-07-22 22:32:39 +0300

    Moved files from asura hosting

---
+++ wp-admin/widgets.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Redirecting...</title>
+		<meta http-equiv="refresh" content="0;url=http://localhost/nikos/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fnikos%2Fwp-admin%2Fwidgets.php&reauth=1">
+	</head>
+	<body>
+		<script type="text/javascript">
+			window.location = "http://localhost/nikos/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fnikos%2Fwp-admin%2Fwidgets.php&reauth=1";
+		</script>
+
+		<p>You are being redirected to <a href="http://localhost/nikos/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fnikos%2Fwp-admin%2Fwidgets.php&reauth=1">http://localhost/nikos/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fnikos%2Fwp-admin%2Fwidgets.php&reauth=1</a></p>
+	</body>
+</html>
